399d7bce558f7b4bad2f02c55f246e95a0555b5e
carfox <carlotreball@gmail.com>
2025-01-04 17:01:38 -0500

    fix ByFileComponent; enhance file upload handling and improve code structure

--- src/app/features/data-upload/components/by-file/by-file.component.ts
+++ src/app/features/data-upload/components/by-file/by-file.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataSharingService } from 'app/shared/services/data-sharing.service';
 import { MessageService } from 'primeng/api';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
 import { FileUpload, } from 'primeng/fileupload';
-import { ProgressBar } from 'primeng/progressbar';
-import {ToastModule } from 'primeng/toast';
+import { ProgressBar, ProgressBarModule } from 'primeng/progressbar';
+import { Toast, ToastModule } from 'primeng/toast';
 interface UploadEvent {
   originalEvent: Event;
   files: File[];
@@ -22,6 +24,7 @@ interface UploadEvent {
     BadgeModule,
     ProgressBar,
     ToastModule,
+    HttpClientModule,
     CommonModule,
   ],
   providers: [MessageService],
@@ -32,7 +35,9 @@ interface UploadEvent {
 export class ByFileComponent {
   uploadedFiles: any[] = [];
 
-  constructor(private router: Router, private messageService: MessageService) {}
+  constructor(
+    private router: Router,
+    private messageService: MessageService) {}
   private dataSharingService = inject(DataSharingService);
 
   onUpload(event: any) {
@@ -40,7 +45,7 @@ export class ByFileComponent {
       this.uploadedFiles.push(file);
     }
     console.log('archivo cargado', event.files[0]);
-    this.dataSharingService.updateTable(event.files[0]);
+    this.dataSharingService.updateTable(this.uploadedFiles[0]);
 
     this.messageService.add({
       severity: 'info',
@@ -52,4 +57,5 @@ export class ByFileComponent {
   goToPreview() {
     this.router.navigate(['/a/preview']);
   }
+
 }
