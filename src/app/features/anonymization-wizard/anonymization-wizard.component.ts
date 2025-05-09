f99b8478337f6feb42086b6edbabecdf4030e6c0
Carfox <carlotreball@gmail.com>
2025-05-09 18:21:17 -0500

    feat: actualizar dependencias e integrar Tailwind CSS con PrimeNG
    - Se eliminó `primeflex` y se añadieron `tailwindcss` y `tailwindcss-primeui` en `package.json`.
    - Se configuraron los ajustes de `zIndex` para PrimeNG en `app.component.ts`.
    - Se actualizó `app.config.ts` para mejorar la configuración del tema utilizando capas de Tailwind CSS.
    - Se refactorizó `anonymization-wizard.component.ts` para mejorar el diseño y los estilos.
    - Se mejoró `upload-step.component.ts` con clases de Tailwind CSS para una mejor interfaz.
    - Se limpió `index.html` eliminando clases innecesarias del tag `<body>`.
    - Se amplió `styles.css` para incluir capas de Tailwind CSS y estilos personalizados.
    - Se añadió `tailwind.config.js` para la configuración de Tailwind CSS.

--- src/app/features/anonymization-wizard/anonymization-wizard.component.ts
+++ src/app/features/anonymization-wizard/anonymization-wizard.component.ts
@@ -32,20 +32,28 @@ import { ResultStepComponent } from "./components/result-step.component";
       <p-step-panels>
         <p-step-panel [value]="1">
           <ng-template #content let-activateCallback="activateCallback">
-            <div class="flex flex-col h-48">
-              <div
-                class="border-2 border-dashed border-surface-200 dark:border-surface-700 rounded bg-surface-50 dark:bg-surface-950 flex-auto flex justify-center items-center font-medium"
-              >
-                <aw-upload-step></aw-upload-step>
-              </div>
-              </div>
-              <div class="flex pt-6 justify-start">
-                <p-button
-                  label="Next"
-                  icon="pi pi-arrow-right"
-                  iconPos="right"
-                  (onClick)="activateCallback(2)"
-                />
+                        <div class="flex flex-col justify-between min-h-[60vh] w-full">
+                  <div class="bg-surface-0 dark:bg-surface-900 p-6 shadow rounded-border">
+                    <div class="text-3xl font-medium text-surface-900 dark:text-surface-0 mb-4">
+                      Carga de Datos
+                    </div>
+                    <div class="font-medium text-surface-500 dark:text-surface-300 mb-4">
+                      Puedes cargar un archivo CSV o Excel para iniciar el proceso de
+                      Anonimización. Asegúrate de que el archivo esté en el formato
+                      correcto y contenga los datos necesarios.
+                    </div>
+                    <div class="border-2 border-dashed border-surface min-h-[45vh]">
+                      <aw-upload-step></aw-upload-step>
+                    </div>
+                  </div>
+            <div class="flex pt-5 justify-center">
+              <p-button
+                label="Siguiente"
+                icon="pi pi-arrow-right"
+                iconPos="right"
+                (onClick)="activateCallback(2)"
+              />
+            </div>
             </div>
           </ng-template>
         </p-step-panel>
@@ -59,7 +67,7 @@ import { ResultStepComponent } from "./components/result-step.component";
                 <aw-preview-step></aw-preview-step>
               </div>
             </div>
-            <div class="flex pt-6 justify-between">
+            <div class="flex pt-12 justify-between">
               <p-button
                 label="Back"
                 severity="secondary"
@@ -85,7 +93,7 @@ import { ResultStepComponent } from "./components/result-step.component";
                 <aw-identifiers-step></aw-identifiers-step>
               </div>
             </div>
-            <div class="flex pt-6 justify-between">
+            <div class="flex pt-12 justify-between">
               <p-button
                 label="Back"
                 severity="secondary"
@@ -110,7 +118,7 @@ import { ResultStepComponent } from "./components/result-step.component";
                 <aw-anonymize-step></aw-anonymize-step>>
               </div>
             </div>
-            <div class="flex pt-6 justify-between">
+            <div class="flex pt-12 justify-between">
               <p-button
                 label="Back"
                 severity="secondary"
@@ -136,7 +144,7 @@ import { ResultStepComponent } from "./components/result-step.component";
               </div>
             </div>
 
-            <div class="flex pt-6 justify-between">
+            <div class="flex pt-12 justify-between">
               <p-button
                 label="Back"
                 severity="secondary"
