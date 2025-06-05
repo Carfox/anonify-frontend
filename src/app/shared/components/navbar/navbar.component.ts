4dee5f09b79657f1f3346d561b08b0c51d7b8d66
José Jiménez <jose.jimenez04@epn.edu.ec>
2025-06-04 19:21:29 -0500

    arreglado error de navbar y agregada la funcionalida de cerrado al hacer click fuera del sidebar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  HostListener,
   OnInit,
   Renderer2,
   ViewChild,
@@ -22,6 +23,23 @@ export class NavbarComponent implements OnInit, AfterViewInit {
   @ViewChild('btnSidebarToggler') btnSidebarToggler!: ElementRef<HTMLElement>;
   @ViewChild('navClosed') navClosed!: ElementRef<HTMLElement>;
   @ViewChild('navOpen') navOpen!: ElementRef<HTMLElement>;
+  
+  // @ViewChild('si') dropdownRef!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.sidebar.nativeElement.contains(event.target as Node) || this.navbar.nativeElement.contains(event.target as Node);
+    if (!clickedInside) {
+      // Acción al hacer clic fuera
+      console.log('Clic fuera del componente sidebar y navbar');
+      if (this.sidebarVisible) {
+      
+      this.closeSidebar(event);
+      }
+
+      // Puedes cerrar un menú, ocultar un panel, etc.
+    }
+  }
   private sidebarVisible = false;
 
   constructor(private renderer: Renderer2, private router: Router) {}
@@ -47,17 +65,25 @@ export class NavbarComponent implements OnInit, AfterViewInit {
     console.log('El valor de sidebarVisible es:', this.sidebarVisible);
     if (this.sidebarVisible) {
       this.renderer.addClass(this.sidebar.nativeElement, 'show');
-      this.renderer.removeClass(this.navClosed.nativeElement, 'hidden');
-      this.renderer.addClass(this.navOpen.nativeElement, 'hidden');
+      this.renderer.addClass(this.navClosed.nativeElement, 'hidden');
+      this.renderer.removeClass(this.navOpen.nativeElement, 'hidden');
       console.log('test1');
     } else {
       
       this.renderer.removeClass(this.sidebar.nativeElement, 'show');
-      this.renderer.addClass(this.navClosed.nativeElement, 'hidden');
-      this.renderer.removeClass(this.navOpen.nativeElement, 'hidden');
+      this.renderer.removeClass(this.navClosed.nativeElement, 'hidden');
+      this.renderer.addClass(this.navOpen.nativeElement, 'hidden');
       console.log('test1 else');
     }
 
+  }
+  closeSidebar(event: Event): void {
+    event.preventDefault();
+    this.sidebarVisible = false;
+    this.renderer.removeClass(this.sidebar.nativeElement, 'show');
+    this.renderer.removeClass(this.navClosed.nativeElement, 'hidden');
+    this.renderer.addClass(this.navOpen.nativeElement, 'hidden');
+    console.log('El valor de sidebarVisible es:', this.sidebarVisible);
   }
   navigateTo(event: Event, route: string): void {
     event.preventDefault();
